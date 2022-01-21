0bc04777e0203d9135d4f00c46d663a6e4d09d14
Iain Scott <iscott@cantab.net>
2022-01-21 14:21:33 +0000

    Remove all the flags which are hopefully not necessary.

--- symbolica.sh
+++ symbolica.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env sh
 set -e
 
-PATH="$(eval echo ~/.symbolica/bin):$PATH" \
-CODEGEN=1 CC=~/.symbolica/bin/cc CFLAGS="-O0 -Xclang -disable-O0-optnone -fPIC" CXX=~/.symbolica/bin/cxx CXXFLAGS="-O0 -Xclang -disable-O0-optnone -fPIC -fno-threadsafe-statics" \
-make OBJCACHE= OPT_GLOBAL=-O0 OPT_FAST=-O0 OPT_SLOW=-O0 SIMULATOR=verilator sim
+CODEGEN=1 CXXFLAGS="-fno-threadsafe-statics" PATH="$(eval echo ~/.symbolica/bin):$PATH" make OBJCACHE= OPT_GLOBAL=-O0 OPT_FAST=-O0 OPT_SLOW=-O0 SIMULATOR=verilator sim
 find hardware/simulation/verilator/obj_dir -name "*.o.bc" -print0 | xargs -0 ~/.symbolica/bin/libcxx-link -o symbolica.bc
