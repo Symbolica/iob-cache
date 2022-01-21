4a1fcc4a7382a4adedc7fbf80355c89a1221c471
Iain Scott <iscott@cantab.net>
2022-01-21 12:07:30 +0000

    Add symbolica shell script.

---
+++ symbolica.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+set -e
+
+PATH="$(eval echo ~/.symbolica/bin):$PATH" \
+CODEGEN=1 CC=~/.symbolica/bin/cc CFLAGS="-O0 -Xclang -disable-O0-optnone -fPIC" CXX=~/.symbolica/bin/cxx CXXFLAGS="-O0 -Xclang -disable-O0-optnone -fPIC -fno-threadsafe-statics" \
+OBJCACHE= OPT_GLOBAL=-O0 OPT_FAST=-O0 SIMULATOR=verilator make sim
+find hardware/simulation/verilator/obj_dir -name "*.o.bc" -print0 | xargs -0 ~/.symbolica/bin/libcxx-link -o symbolica.bc
