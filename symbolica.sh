9bae1ef5ab63ec38eb5cba562f6747bb203875c1
Iain Scott <iscott@cantab.net>
2022-01-21 14:50:55 +0000

    Symbolica shell (#3)

---
+++ symbolica.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+set -e
+
+rm -rf hardware/simulation/verilator/obj_dir
+
+CODEGEN=1 CXXFLAGS="-fno-threadsafe-statics" PATH="$(eval echo ~/.symbolica/bin):$PATH" make OBJCACHE= OPT_GLOBAL=-O0 OPT_FAST=-O0 OPT_SLOW=-O0 SIMULATOR=verilator sim
+find hardware/simulation/verilator/obj_dir -name "*.o.bc" -print0 | xargs -0 ~/.symbolica/bin/libcxx-link -o symbolica.bc
