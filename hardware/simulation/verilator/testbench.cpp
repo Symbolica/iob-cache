76d30a08e997d6dfd028fb4bd97914b4b4c9bc85
Iain Scott <iscott@cantab.net>
2022-01-27 22:45:37 +0000

    Symbolize everything for one loop iteration.

--- hardware/simulation/verilator/testbench.cpp
+++ hardware/simulation/verilator/testbench.cpp
@@ -1,7 +1,7 @@
-#include <verilated.h>
+#include <symbolica.h>
 #include "obj_dir/Viob_cache.h"
 
-#define NUM_INPUT_SET 10
+#define NUM_INPUT_SET 1
 
 int main(int argc, char **argv)
 {
@@ -9,9 +9,7 @@ int main(int argc, char **argv)
 
     top->clk = 0;
 
-    // reset
     top->reset = 1;
-
     for (int i = 0; i < 10; i++)
     {
         top->clk = !top->clk;
@@ -28,20 +26,39 @@ int main(int argc, char **argv)
     top->ready = 0;
     for (int i = 0; i < NUM_INPUT_SET; i++)
     {
-        // hook up to tools...
-        // 0 <= addr <= 4095
-        // wdata: 4-byte long integer
-        // 0 <= wstrb <= 15
-        // valid: boolean
-        top->addr = 0;
-        top->wdata = 0;
-        top->wstrb = 0;
-        top->valid = true;
+        vluint32_t addr;
+        vluint32_t wdata;
+        vluint8_t wstrb;
+        vluint8_t valid;
+
+        SYMBOLIZE(addr);
+        SYMBOLIZE(wdata);
+        SYMBOLIZE(wstrb);
+        SYMBOLIZE(valid);
+
+        if (addr < 0 || addr > 4095)
+        {
+            return 0;
+        }
+
+        if (wstrb < 0 || wstrb > 15)
+        {
+            return 0;
+        }
+
+        if (valid < 0 || valid > 1)
+        {
+            return 0;
+        }
+
+        top->addr = addr;
+        top->wdata = wdata;
+        top->wstrb = wstrb;
+        top->valid = valid;
 
         int j = 0;
         while (top->ready == 0 && ++j < 30)
         {
-            // Toggle a clock
             top->clk = !top->clk;
             top->eval();
         }
